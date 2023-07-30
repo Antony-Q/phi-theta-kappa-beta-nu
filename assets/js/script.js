cbb1e572db96809a5b160e6c1e0027000ae40452
Antony-Q <antonyq1992@gmail.com>
2023-07-30 09:43:04 -0500

    a lot of changes to navbar. almost all desired functionality, appeal, and features are present. adding hamburger collapse to finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-require('bootstrap');
+$(document).ready(function () {
+    // Function to update the navbar based on screen size
+    function updateNavbar() {
+      if ($(window).width() < 992) {
+        $('.navbar-collapse').addClass('collapse');
+        $('.navbar-toggler').show();
+      } else {
+        $('.navbar-collapse').removeClass('collapse');
+        $('.navbar-toggler').hide();
+      }
+    }
+  
+    // Call the function on page load and window resize
+    $(window).on('load resize', updateNavbar);
+  
+    // Toggle the collapsed class when the toggler button is clicked
+    $('.navbar-toggler').on('click', function () {
+      $('.navbar-collapse').toggleClass('collapse');
+    });
+  });
+  
